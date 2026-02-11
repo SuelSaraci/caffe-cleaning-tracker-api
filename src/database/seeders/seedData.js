7f6a676c6304c9bdf1bcf82f2b6bcb375dd7ed0c
Suel Saraçi <s.saraci@infinitas-security.com>
2026-02-11 12:00:53 +0100

    x

--- src/database/seeders/seedData.js
+++ src/database/seeders/seedData.js
@@ -1,52 +1,256 @@
 /** Initial cafe locations - same as client/src/data/locations.ts */
 export const rawLocations = [
-  { Name: "Lejza F.k", Location: "https://maps.app.goo.gl/gHqSYseznFw21vPX8", City: "Fushe Kosove" },
-  { Name: "Fron FK", Location: "https://maps.google.com/?q=42.640335,21.095585", City: "Fushe Kosove" },
-  { Name: "City F.k", Location: "https://maps.google.com/?q=42.643398,21.095675", City: "Fushe Kosove" },
-  { Name: "Daily Lipjan", Location: "https://maps.app.goo.gl/tvgrS3hEFkR2jVcj7?g_st=iw", City: "Lipjan" },
-  { Name: "Relax Caffe", Location: "https://maps.app.goo.gl/GFonBRHsEsyQTWcA9?g_st=iw", City: "Lipjan" },
-  { Name: "N'Lagje Lipjan", Location: "https://maps.app.goo.gl/vDV9QqVTs9pLuswv9?g_st=iw", City: "Lipjan" },
-  { Name: "Fin Caffe Hajvali", Location: "https://maps.app.goo.gl/rHL3mAW9u4Fcgcw5A?g_st=iw", City: "Hajvali" },
-  { Name: "London Bardhosh", Location: "https://maps.google.com/?q=42.712738,21.151247", City: "Prishtinë" },
-  { Name: "Batatina", Location: "https://maps.google.com/?q=42.685741,21.159027", City: "Prishtinë" },
-  { Name: "Arbri Vranjevc", Location: "https://maps.google.com/?q=42.686508,21.160633", City: "Prishtinë" },
-  { Name: "Point Bar Vranjevc", Location: "https://maps.google.com/?q=42.677670,21.162428", City: "Prishtinë" },
-  { Name: "Alo Café Vranjevc", Location: "https://maps.google.com/?q=42.676647,21.164980", City: "Prishtinë" },
-  { Name: "Daily Bar Kolovice", Location: "https://maps.google.com/?q=42.668446,21.169622", City: "Prishtinë" },
-  { Name: "Rafeti N'pike Medrese", Location: "https://maps.google.com/?q=42.674690,21.168169", City: "Prishtinë" },
-  { Name: "Dani Caffe Kolovice", Location: "https://maps.google.com/?q=42.671391,21.169544", City: "Prishtinë" },
-  { Name: "Burger Caffe Medrese", Location: "https://maps.google.com/?q=42.677097,21.170895", City: "Prishtinë" },
-  { Name: "N'lagje te Pazari", Location: "https://maps.google.com/?q=42.670742,21.168015", City: "Prishtinë" },
-  { Name: "Str8 te parku", Location: "https://www.google.com/maps/place/Varri+i+Ibrahim+Rugova/@42.6606818,21.1669148,49m/data=!3m1!1e3!4m6!3m5!1s0x13549f953fae1c51:0xf0965f7b3006397d!8m2!3d42.6587085!4d21.1763329!16s%2Fg%2F11r_w8_4h2?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D", City: "Prishtinë" },
-  { Name: "French Taccos", Location: "https://www.google.com/maps/place/French+Tacos/@42.6567941,21.1623308,787m/data=!3m2!1e3!4b1!4m6!3m5!1s0x13549fadfac8f04d:0xd69151b9efe1d7e0!8m2!3d42.6567902!4d21.1649057!16s%2Fg%2F11gxnw3jbw?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D", City: "Prishtinë" },
-  { Name: "Coffe Time Matiqan", Location: "https://maps.app.goo.gl/1FFRysuySuviJkNEA?g_st=iw", City: "Prishtinë" },
-  { Name: "Villa Panorama", Location: "https://maps.app.goo.gl/PnqcpsaEceaoUBkT8?g_st=iw", City: "Prishtinë" },
-  { Name: "Blue 99", Location: "https://maps.google.com/?q=42.654694,21.178930", City: "Prishtinë" },
-  { Name: "Zoom te maxi 24 rruga b", Location: "https://www.google.com/maps/place/Maxi+Supermarket+8+-+24h/@42.652565,21.1737393,787m/data=!3m2!1e3!4b1!4m6!3m5!1s0x13549edfe9f358ed:0xfa8c316bcfaf4afc!8m2!3d42.6525611!4d21.1763142!16s%2Fg%2F11h03dmgw?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D", City: "Prishtinë" },
-  { Name: "Louvard", Location: "https://maps.google.com/?q=42.654694,21.178930", City: "Prishtinë" },
-  { Name: "Vertigo Bregu i diellit", Location: "https://www.google.com/maps/place/Vertigo+Home/@42.6523285,21.1693107,164m/data=!3m1!1e3!4m6!3m5!1s0x13549fed3dd74437:0xa849a26845676c3a!8m2!3d42.6523944!4d21.1693588!16s%2Fg%2F11swkdyl4j?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D", City: "Prishtinë" },
-  { Name: "Sun Caffe Te aleanca", Location: "https://www.google.com/maps/place/Ulpiane/@42.6530831,21.1583832,157m/data=!3m1!1e3!4m10!1m2!2m1!1saleanca+ulpian!3m6!1s0x13549eeeb6acf4e1:0x521db695e6cd3fce!8m2!3d42.65293!4d21.158905!15sCg5hbGVhbmNhIHVscGlhbpIBCGJ1c19zdG9w4AEA!16s%2Fg%2F11hz5rwjrv?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D", City: "Prishtinë" },
-  { Name: "Ambar Caffe", Location: "https://maps.google.com/?q=42.651768,21.152962", City: "Prishtinë" },
-  { Name: "N'Soliter", Location: "https://www.google.com/maps/place/N'Soliter/@42.6513285,21.1573039,787m/data=!3m2!1e3!4b1!4m6!3m5!1s0x13549fb3115dc89d:0x610a88a81f1ad5a9!8m2!3d42.6513247!4d21.1621748!16s%2Fg%2F11kt5r37f_?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D", City: "Prishtinë" },
-  { Name: "Tron Caffe Ulpiane", Location: "https://maps.google.com/?q=42.649647,21.163702", City: "Prishtinë" },
-  { Name: "Matis Caffe Santea", Location: "https://maps.google.com/?q=42.653721,21.156128", City: "Prishtinë" },
-  { Name: "Rogos Sante", Location: "https://maps.google.com/?q=42.654564,21.156931", City: "Prishtinë" },
-  { Name: "Rogos Zalli", Location: "https://maps.google.com/?q=42.657204,21.152063", City: "Prishtinë" },
-  { Name: "Frame Pejton", Location: "https://maps.google.com/?q=42.658360,21.156502", City: "Prishtinë" },
-  { Name: "Almore Tophane", Location: "https://www.google.com/maps/place/Almora+Caffe/@42.6688373,21.157828,210m/data=!3m1!1e3!4m6!3m5!1s0x13549f1e4d6f0765:0x7ee94101985c28f1!8m2!3d42.6688768!4d21.1583819!16s%2Fg%2F11krgrlnn4?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D", City: "Prishtinë" },
-  { Name: "Sucre Tophane", Location: "https://www.google.com/maps/place/Tophane,+Pristina+10000/@42.668754,21.1600758,49m/data=!3m1!1e3!4m6!3m5!1s0x13549f1e7e033e69:0x40fd9457e169c81c!8m2!3d42.6684539!4d21.1611114!16s%2Fg%2F1th842zt?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D", City: "Prishtinë" },
-  { Name: "Novus Bar", Location: "https://maps.google.com/?q=42.657497,21.146595", City: "Prishtinë" },
-  { Name: "Hilton", Location: "https://www.google.com/maps/place/Qafa,+Pristina+10000/@42.6653779,21.159151,83m/data=!3m1!1e3!4m6!3m5!1s0x13549ee23b73d513:0x759ac60452a08a3!8m2!3d42.6648026!4d21.1593651!16s%2Fg%2F1tf554r6?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D", City: "Prishtinë" },
-  { Name: "Prime Bar", Location: "https://www.google.com/maps/place/PRIME+Lounge+%26+Bar/@42.6574042,21.1318323,91m/data=!3m1!1e3!4m14!1m7!3m6!1s0x13549e59716b9d79:0x431c628120701c83!2sTe+Ismeti!8m2!3d42.6584171!4d21.1365207!16s%2Fg%2F1tg97ytj!3m5!1s0x13549f8cd21f01e7:0x4761eefc8f597af1!8m2!3d42.6572045!4d21.1318368!16s%2Fg%2F11hxys28l8?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D", City: "Prishtinë" },
-  { Name: "Ame Vushtrri", Location: "https://www.google.com/maps/place/Pizzeria+AME/@42.8361541,20.9594313,176m/data=!3m1!1e3!4m6!3m5!1s0x13535b1d9ba88169:0x733da3f3f932b3a5!8m2!3d42.83608!4d20.9598492!16s%2Fg%2F11v41gnkps?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D", City: "Vushtrri" },
-  { Name: "Friends Caffe Mitrovicë", Location: "https://maps.app.goo.gl/YC3gkrhL3eeLA1B89", City: "Mitrovicë" },
-  { Name: "Mega Center", Location: "https://maps.app.goo.gl/SsvPgcQtogpQ248p7?g_st=iw", City: "Mitrovicë" },
-  { Name: "Rida", Location: "https://maps.app.goo.gl/ot2uR5YU7gwL6E4a7?g_st=iw", City: "Mitrovicë" },
-  { Name: "Inna Caffe Podujeve", Location: "https://maps.app.goo.gl/XCBHJ3ThQk7FNdRj6?g_st=iw", City: "Podujevë" },
-  { Name: "Te Liqeni Batllav", Location: "https://maps.app.goo.gl/xK732f61BR9vNDcE7", City: "Podujevë" },
-  { Name: "Panorama Vranjevc (the view)", Location: "https://maps.app.goo.gl/gSSA7xWgo7NvtRDw6", City: "Prishtinë" },
-  { Name: "Sonder", Location: "https://maps.google.com/?q=42.655033,21.165897", City: "Prishtinë" },
+  {
+    Name: "Lejza F.k",
+    Location: "https://maps.app.goo.gl/gHqSYseznFw21vPX8",
+    City: "Fushe Kosove",
+  },
+  {
+    Name: "Fron FK",
+    Location: "https://maps.google.com/?q=42.640335,21.095585",
+    City: "Fushe Kosove",
+  },
+  {
+    Name: "City F.k",
+    Location: "https://maps.google.com/?q=42.643398,21.095675",
+    City: "Fushe Kosove",
+  },
+  {
+    Name: "Daily Lipjan",
+    Location: "https://maps.app.goo.gl/tvgrS3hEFkR2jVcj7?g_st=iw",
+    City: "Lipjan",
+  },
+  {
+    Name: "Relax Caffe",
+    Location: "https://maps.app.goo.gl/GFonBRHsEsyQTWcA9?g_st=iw",
+    City: "Lipjan",
+  },
+  {
+    Name: "N'Lagje Lipjan",
+    Location: "https://maps.app.goo.gl/vDV9QqVTs9pLuswv9?g_st=iw",
+    City: "Lipjan",
+  },
+  {
+    Name: "Fin Caffe Hajvali",
+    Location: "https://maps.app.goo.gl/rHL3mAW9u4Fcgcw5A?g_st=iw",
+    City: "Hajvali",
+  },
+  {
+    Name: "London Bardhosh",
+    Location: "https://maps.google.com/?q=42.712738,21.151247",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Batatina",
+    Location: "https://maps.google.com/?q=42.685741,21.159027",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Arbri Vranjevc",
+    Location: "https://maps.google.com/?q=42.686508,21.160633",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Point Bar Vranjevc",
+    Location: "https://maps.google.com/?q=42.677670,21.162428",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Alo Café Vranjevc",
+    Location: "https://maps.google.com/?q=42.676647,21.164980",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Daily Bar Kolovice",
+    Location: "https://maps.google.com/?q=42.668446,21.169622",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Rafeti N'pike Medrese",
+    Location: "https://maps.google.com/?q=42.674690,21.168169",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Dani Caffe Kolovice",
+    Location: "https://maps.google.com/?q=42.671391,21.169544",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Burger Caffe Medrese",
+    Location: "https://maps.google.com/?q=42.677097,21.170895",
+    City: "Prishtinë",
+  },
+  {
+    Name: "N'lagje te Pazari",
+    Location: "https://maps.google.com/?q=42.670742,21.168015",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Str8 te parku",
+    Location:
+      "https://www.google.com/maps/place/Varri+i+Ibrahim+Rugova/@42.6606818,21.1669148,49m/data=!3m1!1e3!4m6!3m5!1s0x13549f953fae1c51:0xf0965f7b3006397d!8m2!3d42.6587085!4d21.1763329!16s%2Fg%2F11r_w8_4h2?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D",
+    City: "Prishtinë",
+  },
+  {
+    Name: "French Taccos",
+    Location:
+      "https://www.google.com/maps/place/French+Tacos/@42.6567941,21.1623308,787m/data=!3m2!1e3!4b1!4m6!3m5!1s0x13549fadfac8f04d:0xd69151b9efe1d7e0!8m2!3d42.6567902!4d21.1649057!16s%2Fg%2F11gxnw3jbw?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Coffe Time Matiqan",
+    Location: "https://maps.app.goo.gl/1FFRysuySuviJkNEA?g_st=iw",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Villa Panorama",
+    Location: "https://maps.app.goo.gl/PnqcpsaEceaoUBkT8?g_st=iw",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Blue 99",
+    Location: "https://maps.google.com/?q=42.654694,21.178930",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Zoom te maxi 24 rruga b",
+    Location:
+      "https://www.google.com/maps/place/Maxi+Supermarket+8+-+24h/@42.652565,21.1737393,787m/data=!3m2!1e3!4b1!4m6!3m5!1s0x13549edfe9f358ed:0xfa8c316bcfaf4afc!8m2!3d42.6525611!4d21.1763142!16s%2Fg%2F11h03dmgw?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Louvard",
+    Location: "https://maps.google.com/?q=42.654694,21.178930",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Vertigo Bregu i diellit",
+    Location:
+      "https://www.google.com/maps/place/Vertigo+Home/@42.6523285,21.1693107,164m/data=!3m1!1e3!4m6!3m5!1s0x13549fed3dd74437:0xa849a26845676c3a!8m2!3d42.6523944!4d21.1693588!16s%2Fg%2F11swkdyl4j?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Sun Caffe Te aleanca",
+    Location:
+      "https://www.google.com/maps/place/Ulpiane/@42.6530831,21.1583832,157m/data=!3m1!1e3!4m10!1m2!2m1!1saleanca+ulpian!3m6!1s0x13549eeeb6acf4e1:0x521db695e6cd3fce!8m2!3d42.65293!4d21.158905!15sCg5hbGVhbmNhIHVscGlhbpIBCGJ1c19zdG9w4AEA!16s%2Fg%2F11hz5rwjrv?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Ambar Caffe",
+    Location: "https://maps.google.com/?q=42.651768,21.152962",
+    City: "Prishtinë",
+  },
+  {
+    Name: "N'Soliter",
+    Location:
+      "https://www.google.com/maps/place/N'Soliter/@42.6513285,21.1573039,787m/data=!3m2!1e3!4b1!4m6!3m5!1s0x13549fb3115dc89d:0x610a88a81f1ad5a9!8m2!3d42.6513247!4d21.1621748!16s%2Fg%2F11kt5r37f_?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Tron Caffe Ulpiane",
+    Location: "https://maps.google.com/?q=42.649647,21.163702",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Matis Caffe Santea",
+    Location: "https://maps.google.com/?q=42.653721,21.156128",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Rogos Sante",
+    Location: "https://maps.google.com/?q=42.654564,21.156931",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Rogos Zalli",
+    Location: "https://maps.google.com/?q=42.657204,21.152063",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Frame Pejton",
+    Location: "https://maps.google.com/?q=42.658360,21.156502",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Almore Tophane",
+    Location:
+      "https://www.google.com/maps/place/Almora+Caffe/@42.6688373,21.157828,210m/data=!3m1!1e3!4m6!3m5!1s0x13549f1e4d6f0765:0x7ee94101985c28f1!8m2!3d42.6688768!4d21.1583819!16s%2Fg%2F11krgrlnn4?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Sucre Tophane",
+    Location:
+      "https://www.google.com/maps/place/Tophane,+Pristina+10000/@42.668754,21.1600758,49m/data=!3m1!1e3!4m6!3m5!1s0x13549f1e7e033e69:0x40fd9457e169c81c!8m2!3d42.6684539!4d21.1611114!16s%2Fg%2F1th842zt?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Novus Bar",
+    Location: "https://maps.google.com/?q=42.657497,21.146595",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Hilton",
+    Location:
+      "https://www.google.com/maps/place/Qafa,+Pristina+10000/@42.6653779,21.159151,83m/data=!3m1!1e3!4m6!3m5!1s0x13549ee23b73d513:0x759ac60452a08a3!8m2!3d42.6648026!4d21.1593651!16s%2Fg%2F1tf554r6?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Prime Bar",
+    Location:
+      "https://www.google.com/maps/place/PRIME+Lounge+%26+Bar/@42.6574042,21.1318323,91m/data=!3m1!1e3!4m14!1m7!3m6!1s0x13549e59716b9d79:0x431c628120701c83!2sTe+Ismeti!8m2!3d42.6584171!4d21.1365207!16s%2Fg%2F1tg97ytj!3m5!1s0x13549f8cd21f01e7:0x4761eefc8f597af1!8m2!3d42.6572045!4d21.1318368!16s%2Fg%2F11hxys28l8?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Ame Vushtrri",
+    Location:
+      "https://www.google.com/maps/place/Pizzeria+AME/@42.8361541,20.9594313,176m/data=!3m1!1e3!4m6!3m5!1s0x13535b1d9ba88169:0x733da3f3f932b3a5!8m2!3d42.83608!4d20.9598492!16s%2Fg%2F11v41gnkps?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D",
+    City: "Vushtrri",
+  },
+  {
+    Name: "Friends Caffe Mitrovicë",
+    Location: "https://maps.app.goo.gl/YC3gkrhL3eeLA1B89",
+    City: "Mitrovicë",
+  },
+  {
+    Name: "Mega Center",
+    Location: "https://maps.app.goo.gl/SsvPgcQtogpQ248p7?g_st=iw",
+    City: "Podujevë",
+  },
+  {
+    Name: "Rida",
+    Location: "https://maps.app.goo.gl/ot2uR5YU7gwL6E4a7?g_st=iw",
+    City: "Podujevë",
+  },
+  {
+    Name: "Inna Caffe Podujeve",
+    Location: "https://maps.app.goo.gl/XCBHJ3ThQk7FNdRj6?g_st=iw",
+    City: "Podujevë",
+  },
+  {
+    Name: "Te Liqeni Batllav",
+    Location: "https://maps.app.goo.gl/xK732f61BR9vNDcE7",
+    City: "Podujevë",
+  },
+  {
+    Name: "Panorama Vranjevc (the view)",
+    Location: "https://maps.app.goo.gl/gSSA7xWgo7NvtRDw6",
+    City: "Prishtinë",
+  },
+  {
+    Name: "Sonder",
+    Location: "https://maps.google.com/?q=42.655033,21.165897",
+    City: "Prishtinë",
+  },
   { Name: "Batatina 2 Lupq", Location: "Lupq", City: "Prishtinë" },
-  { Name: "Te Shoki Veterrnik", Location: "https://www.google.com/maps/place/42%C2%B037'48.8%22N+21%C2%B009'04.8%22E/@42.6302311,21.1487601,787m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d42.6302311!4d21.151335!18m1!1e1?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D", City: "Veternik" },
-  { Name: "Lon cafe", Location: "https://maps.google.com/?q=42.875008,20.858873", City: "Mitrovicë" },
+  {
+    Name: "Te Shoki Veterrnik",
+    Location:
+      "https://www.google.com/maps/place/42%C2%B037'48.8%22N+21%C2%B009'04.8%22E/@42.6302311,21.1487601,787m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d42.6302311!4d21.151335!18m1!1e1?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D",
+    City: "Veternik",
+  },
+  {
+    Name: "Lon cafe",
+    Location: "https://maps.google.com/?q=42.875008,20.858873",
+    City: "Mitrovicë",
+  },
 ];
